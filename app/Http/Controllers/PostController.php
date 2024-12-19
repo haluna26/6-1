04831201073ecdcd44f07b9fafbe4b45a1c3e008
haluna26 <kimura.syukatsu2004@gmail.com>
2024-12-19 13:27:18 +0000

    Blog slide is created in dv3.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,14 @@ class PostController extends Controller
 {
     return view('posts.index')->with(['posts' => $post->getPaginateByLimit(3)]);
 }
+
+  
+public function show(Post $post)
+{
+    return view('posts.show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+}
+
 }
 
 ?>
